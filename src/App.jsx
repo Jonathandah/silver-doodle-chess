39fc01b2361bbd7acaef8fb785c9daf7b1bf24ac
unknown <jmd119988@gmail.com>
2019-12-18 12:20:30 +0100

    added global, test folder, built a form for lign and added global validation function for all the forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './views/home/Home';
+import Login from './views/login/Login';
+import Register from './views/register/Register';
 import './App.sass';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Global/functions/validateUser/validateUser.js
@@ -0,0 +1,28 @@
+let errors = {};
+
+export default function validateUser(userData) {
+  console.log(userData);
+  // let postalcode = /^[0-9\s]{5,9}$/;
+  // let alphanumeric = /^(?=.*[a-zA-ZåäöÅÄÖ])(?=.*[0-9])/;
+  // let letters = /^[A-zöåäÖÅÄ\s]*$/;
+
+  if (userData.username.length > 4) {
+    delete errors.username;
+  } else {
+    errors = {
+      username: 'Username has to be 4 characters or more.',
+      ...errors
+    };
+  }
+
+  if (userData.password.length > 6) {
+    delete errors.password;
+  } else {
+    errors = {
+      password: 'Password has to be 6 characters or more.',
+      ...errors
+    };
+  }
+
+  return errors;
+}
