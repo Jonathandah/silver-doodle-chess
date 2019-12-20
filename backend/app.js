5a71a7bff0e2dce478f2d1f84de55116522546f3
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-12-20 12:07:14 +0100

    added chai-http to test endpoints, added test example

--- backend/app.js
+++ backend/app.js
@@ -7,48 +7,52 @@ const io = require('socket.io')(http);
 const users = require('./databas/users.json');
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const STORAGE_USERS = './databas/users.json';
 
-
 /************** REGISTER **************/
 
-app.post('/api/register', function(req, res) {  
-
+app.post('/api/register', function(req, res) {
   if (!req.body.username && !req.body.password) {
     res.status(400).end();
     return;
   }
 
   if (req.body.username.length < 4 || req.body.password.length < 6) {
-    res.status(400).send('Username must contain atleast 4 characters and password must have minimum 6 characters.');
+    res
+      .status(400)
+      .send(
+        'Username must contain atleast 4 characters and password must have minimum 6 characters.'
+      );
     return;
-  }   
+  }
 
-    fs.readFile(STORAGE_USERS, function(error, data) {
-      if(error) {
-        res.status(500).send('Server Error: Could not create new user')
+  fs.readFile(STORAGE_USERS, function(error, data) {
+    if (error) {
+      res.status(500).send('Server Error: Could not create new user');
+      return;
+    }
+
+    let users = JSON.parse(data);
+
+    for (let user in users) {
+      if (users[user].username === req.body.username) {
+        res.status(400).send('A user with this name already exists');
         return;
       }
+    }
 
-      let users = JSON.parse(data);
-      
-      for (let user in users) {
-        if (users[user].username === req.body.username) {
-          res.status(400).send('A user with this name already exists');
-          return;
-        }
+    fs.writeFile(
+      STORAGE_USERS,
+      JSON.stringify({ ...users, [+new Date()]: req.body }),
+      function(error) {
+        console.log(JSON.stringify({ ...users, [new Date()]: req.body }));
+        res.status(201).end();
       }
-
-      fs.writeFile(STORAGE_USERS, JSON.stringify({...users, [+new Date()]: req.body}), function(error) {
-          console.log(JSON.stringify({...users, [new Date()]: req.body}));
-          res.status(201).end();
-        });
-    });
-  }
-);
-
+    );
+  });
+});
 
 /************** LOGIN **************/
 
@@ -59,14 +63,18 @@ app.post('/api/login', function(req, res) {
     res.status(400).end();
     return;
   }
-  
+
   if (req.body.username.length < 4 || req.body.password.length < 6) {
-    res.status(400).send('Username must contain atleast 4 characters and password must have minimum 6 characters.');
+    res
+      .status(400)
+      .send(
+        'Username must contain atleast 4 characters and password must have minimum 6 characters.'
+      );
     return;
-  } 
+  }
 
   fs.readFile(STORAGE_USERS, function(error, data) {
-    if(error) {
+    if (error) {
       res.status(500).end();
       return;
     }
@@ -75,52 +83,37 @@ app.post('/api/login', function(req, res) {
 
     for (let user in users) {
       if (JSON.stringify(users[user]) === JSON.stringify(req.body)) {
-        res.status(200).send({[user]: users[user]})
+        res.status(200).send({ [user]: users[user] });
         return;
       }
     }
 
-    res.status(400).send("Username or password is incorrect.");
+    res.status(400).send('Username or password is incorrect.');
   });
-
 });
-
 
 /************** GET ALL GAMES **************/
 
-app.get('/api/games', function(req, res) {
-
-});
-
+app.get('/api/games', function(req, res) {});
 
 /************** ADD A NEW GAME **************/
 
-app.post('/api/games', function(req, res) {
-
-});
-
+app.post('/api/games', function(req, res) {});
 
 /************** ACCEPT/START A GAME **************/
 
-app.post('/api/game/:id', function(req, res) {
-
-});
-
+app.post('/api/game/:id', function(req, res) {});
 
 /************** GET A SPECIFIC GAME **************/
 
-app.get('/api/game/:id', function(req, res) {
-
-});
-
+app.get('/api/game/:id', function(req, res) {});
 
 /************** MAKE A MOVE IN A GAME **************/
 
-app.post('/api/game/move', function(req, res) {
+app.post('/api/game/move', function(req, res) {});
 
-});
-
-
-http.listen(8000, function() {  
+http.listen(8000, function() {
   console.log('Listening on *:8000');
 });
+
+module.exports = http;
