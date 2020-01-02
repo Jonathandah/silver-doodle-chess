0c1187feff96a85c63f1b8fe4609a62e219bd9b1
Yaroslav Lashchenko <Yarik95.95@hotmail.com>
2020-01-02 10:03:38 +0100

    Game endpoints and tests (#20)
    * started structure for endpoints
    
    * added .gitignore for backend, moved chai mocha nodemon to devDependendencies, added test map and script
    
    * added test.js file
    
    * added chai-http to test endpoints, added test example
    
    * register test, check if register is went through or not
    
    * Changed so I can test validation in server
    
    * register test done
    
    * created endpoints to get all games and create new game, created tests for these endpoints
    
    * updated backend/app.js
    
    * updated 'header' validation, moved JSON.parse inside trycatch
    
    Co-authored-by: Joanna Abella <43029517+joannabella@users.noreply.github.com>
    Co-authored-by: Emma Petri <43029545+epetri@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,14 @@ const cors = require('cors');
 const fs = require('fs');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const users = require('./databas/users.json');
+
+const STORAGE_USERS = './databas/users.json';
+const STORAGE_GAMES = './databas/games.json';
 
 app.use(cors());
 app.use(express.json());
 
-const STORAGE_USERS = './databas/users.json';
-
 /************** REGISTER **************/
-
 app.post('/api/register', function(req, res) {
   if (!req.body.username && !req.body.password) {
     res.status(400).end();
@@ -47,7 +46,11 @@ app.post('/api/register', function(req, res) {
       STORAGE_USERS,
       JSON.stringify({ ...users, [+new Date()]: req.body }),
       function(error) {
-        console.log(JSON.stringify({ ...users, [new Date()]: req.body }));
+        if (error) {
+          res.status(500).send('Server Error: Could not create new user');
+          return;
+        }
+
         res.status(201).end();
       }
     );
@@ -55,10 +58,7 @@ app.post('/api/register', function(req, res) {
 });
 
 /************** LOGIN **************/
-
 app.post('/api/login', function(req, res) {
-  console.log(req.body);
-
   if (!req.body.username && !req.body.password) {
     res.status(400).end();
     return;
@@ -79,23 +79,132 @@ app.post('/api/login', function(req, res) {
       return;
     }
 
-    let users = JSON.parse(data);
+    try {
+      let users = JSON.parse(data);
 
-    for (let user in users) {
-      if (JSON.stringify(users[user]) === JSON.stringify(req.body)) {
-        res.status(200).send({ [user]: users[user] });
-        return;
+      for (let user in users) {
+        if (JSON.stringify(users[user]) === JSON.stringify(req.body)) {
+          res.status(200).send({ [user]: users[user] });
+          return;
+        }
       }
+    } catch (error) {
+      console.error(error);
     }
 
+    res.status(400).send('Username or password is incorrect.');
+  });
+});
+
+/************** GET ALL GAMES **************/
+app.get('/api/games', function(req, res) {
+  fs.readFile(STORAGE_GAMES, function(error, formatedData) {
+    if (error) {
+      res.status(500).send('SERVER ERROR: Could not load games');
+      return;
+    }
+
+    try {
+      let data = JSON.parse(formatedData);
+      res.status(200).send(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('SERVER ERROR: Could not parse games');
+    }
+  });
+});
+
+/************** ADD A NEW GAME **************/
+app.post('/api/games', function(req, res) {
+  let data = req.body;
+
+  // checking header
+  if (!data.header) {
+    res.status(400).send('Missing header.');
+    return;
+  }
+
+  if (typeof data.header !== 'object') {
+    res.status(400).send('Header should be an object.');
+    return;
+  }
+
+  if (
+    !data.header.hasOwnProperty('White') ||
+    !data.header.hasOwnProperty('Black') ||
+    !data.header.hasOwnProperty('Date')
+  ) {
     res
       .status(400)
       .send(
-        'Cannot find any user with this combination of username and password.'
+        'Header should contain the following keys: "White", "Black", "Date"'
       );
+    return;
+  }
+
+  // checking board
+  if (!data.board) {
+    res.status(400).send('Missing board.');
+    return;
+  }
+
+  if (typeof data.board !== 'string') {
+    res.status(400).send('Board should be a string.');
+    return;
+  }
+
+  // checking owner
+  if (!data.owner) {
+    res.status(400).send('Missing owner.');
+    return;
+  }
+
+  if (typeof data.owner !== 'string') {
+    res.status(400).send('Owner should be a string.');
+    return;
+  }
+
+  fs.readFile(STORAGE_GAMES, function(error, formatedData) {
+    if (error) {
+      res.status(500).send('Server Error: Could not create new game');
+      return;
+    }
+
+    try {
+      let games = JSON.parse(formatedData);
+
+      fs.writeFile(
+        STORAGE_GAMES,
+        JSON.stringify({ ...games, [+new Date()]: data }),
+        function(error) {
+          if (error) {
+            res.status(500).send('Server Error: Could not create new game');
+            return;
+          }
+
+          res.status(201).end();
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
+
+/************** ACCEPT/START A GAME **************/
+
+app.post('/api/game/:id', function(req, res) {});
+
+/************** GET A SPECIFIC GAME **************/
+
+app.get('/api/game/:id', function(req, res) {});
+
+/************** MAKE A MOVE IN A GAME **************/
+
+app.post('/api/game/move', function(req, res) {});
 
 http.listen(8000, function() {
   console.log('Listening on *:8000');
 });
+
+module.exports = http;
