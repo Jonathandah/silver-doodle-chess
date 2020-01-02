90b64ec668fa09c531a37e8a35c47937c65c9648
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2020-01-02 09:12:33 +0100

    updated 'header' validation, moved JSON.parse inside trycatch

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,12 @@ const cors = require('cors');
 const fs = require('fs');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const users = require('./databas/users.json');
+
+const STORAGE_USERS = './databas/users.json';
+const STORAGE_GAMES = './databas/games.json';
 
 app.use(cors());
 app.use(express.json());
-const STORAGE_USERS = './databas/users.json';
-const STORAGE_GAMES = './databas/games.json';
 
 /************** REGISTER **************/
 app.post('/api/register', function(req, res) {
@@ -58,7 +58,6 @@ app.post('/api/register', function(req, res) {
 });
 
 /************** LOGIN **************/
-
 app.post('/api/login', function(req, res) {
   if (!req.body.username && !req.body.password) {
     res.status(400).end();
@@ -80,13 +79,17 @@ app.post('/api/login', function(req, res) {
       return;
     }
 
-    let users = JSON.parse(data);
+    try {
+      let users = JSON.parse(data);
 
-    for (let user in users) {
-      if (JSON.stringify(users[user]) === JSON.stringify(req.body)) {
-        res.status(200).send({ [user]: users[user] });
-        return;
+      for (let user in users) {
+        if (JSON.stringify(users[user]) === JSON.stringify(req.body)) {
+          res.status(200).send({ [user]: users[user] });
+          return;
+        }
       }
+    } catch (error) {
+      console.error(error);
     }
 
     res.status(400).send('Username or password is incorrect.');
@@ -94,7 +97,6 @@ app.post('/api/login', function(req, res) {
 });
 
 /************** GET ALL GAMES **************/
-
 app.get('/api/games', function(req, res) {
   fs.readFile(STORAGE_GAMES, function(error, formatedData) {
     if (error) {
@@ -102,12 +104,17 @@ app.get('/api/games', function(req, res) {
       return;
     }
 
-    res.status(200).send(JSON.parse(formatedData));
+    try {
+      let data = JSON.parse(formatedData);
+      res.status(200).send(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('SERVER ERROR: Could not parse games');
+    }
   });
 });
 
 /************** ADD A NEW GAME **************/
-
 app.post('/api/games', function(req, res) {
   let data = req.body;
 
@@ -122,7 +129,11 @@ app.post('/api/games', function(req, res) {
     return;
   }
 
-  if (!data.header.White || !data.header.Black || !data.header.Date) {
+  if (
+    !data.header.hasOwnProperty('White') ||
+    !data.header.hasOwnProperty('Black') ||
+    !data.header.hasOwnProperty('Date')
+  ) {
     res
       .status(400)
       .send(
@@ -159,20 +170,24 @@ app.post('/api/games', function(req, res) {
       return;
     }
 
-    let games = JSON.parse(formatedData);
+    try {
+      let games = JSON.parse(formatedData);
 
-    fs.writeFile(
-      STORAGE_GAMES,
-      JSON.stringify({ ...games, [+new Date()]: data }),
-      function(error) {
-        if (error) {
-          res.status(500).send('Server Error: Could not create new game');
-          return;
+      fs.writeFile(
+        STORAGE_GAMES,
+        JSON.stringify({ ...games, [+new Date()]: data }),
+        function(error) {
+          if (error) {
+            res.status(500).send('Server Error: Could not create new game');
+            return;
+          }
+
+          res.status(201).end();
         }
-
-        res.status(201).end();
-      }
-    );
+      );
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
 
