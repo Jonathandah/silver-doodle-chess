37b5e1bf69540805591ff16f3035b32939f554a1
emmapetri <emmapetri@hotmail.com>
2020-01-10 13:33:27 +0100

    commit

--- src/views/game/GameBoard.jsx
+++ src/views/game/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Board from './Board.jsx';
 import Turn from './Turn.jsx';
 import './GameBoard.sass';
