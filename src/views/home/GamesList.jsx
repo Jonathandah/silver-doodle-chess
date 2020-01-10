7ef259bef7dfd97dcd99ca7973e917c9caabb530
Jonathan Meeprong Dahnberg <Jonathan.Dahnberg@cybercom.com>
2020-01-10 10:02:58 +0100

    fixed readabillity in GameList, switched to map and to smaller functions

--- src/views/home/GamesList.jsx
+++ src/views/home/GamesList.jsx
@@ -5,46 +5,52 @@ import axios from "axios"
 import call from "../../global/api/endpoints"
 import { Redirect } from "react-router-dom"
 
-
 const GamesList = ({ games }) => {
   const [joinGame, updateJoinGame] = useState(false)
+
+  const checkJoinability = (gameId, game) => {
+    if ((!game.header.Black || !game.header.White) &&
+      game.owner !== user$.value) {
+      return (
+        <button
+          className="Home__container__list__item__button"
+          onClick={() => {
+            axios.post(call.JOIN_GAME(gameId), { username: user$.value }).then(
+              updateJoinGame(gameId)
+            )
+          }}
+        >
+          Join
+</button>
+      )
+    } else if (game.header.Black === user$.value ||
+      game.header.White === user$.value) {
+      return (
+        <Link to={`/game/${gameId}`}>
+          <button className="Home__container__list__item__button">
+            Play
+    </button>
+        </Link>
+      )
+    }
+  }
+
+  const renderListElements = () => Object.keys(games).map((gameId, idx) => {
+    const game = games[gameId];
+
+    return <li className="Home__container__list__item" key={idx}>
+      <p className="Home__container__list__item__owner">{game.owner}</p>
+      {checkJoinability(gameId, game)}
+    </li>
+  });
+
 
   if (joinGame) {
     return <Redirect to={`/game/${joinGame}`} />
   }
   return (
     <ul className="Home__container__list">
-      {Object.keys(games).reduce((acc, cur, idx) => {
-        const game = games[cur];
-
-        acc.push(
-          <li className="Home__container__list__item" key={idx}>
-            <p className="Home__container__list__item__owner">{game.owner}</p>
-            {(!game.header.Black || !game.header.White) &&
-              game.owner !== user$.value ? (
-                <button
-                  className="Home__container__list__item__button"
-                  onClick={() => {
-                    axios.post(call.JOIN_GAME(cur), { username: user$.value }).then(
-                      updateJoinGame(cur)
-                    )
-                  }}
-                >
-                  Join
-              </button>
-              ) : game.header.Black === user$.value ||
-                game.header.White === user$.value ? (
-                  <Link to={`/game/${cur}`}>
-                    <button className="Home__container__list__item__button">
-                      Play
-                </button>
-                  </Link>
-                ) : null}
-          </li>
-        );
-
-        return acc;
-      }, [])}
+      {renderListElements()}
     </ul>
   );
 };
