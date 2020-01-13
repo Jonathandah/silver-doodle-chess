d1cd31bfed3c59972f9d1f27e425c0738b322660
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2020-01-13 11:25:17 +0100

    Update GameList and MyGames
    Update MyGames component and GameList to show game info correctly,
    removed Game component(no usage)

--- src/views/home/MyGames.jsx
+++ src/views/home/MyGames.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { user$ } from '../../global/store/userStore';
 import { games$, updateGames } from '../../global/store/games';
+import GamesList from './GamesList';
 
 function MyGames() {
   const [games, setGames] = useState(null);
@@ -23,30 +24,17 @@ function MyGames() {
     };
   }, []);
 
-  const renderList = () => {
-    let arr = [];
-
-    for (let gameId in games) {
-      let listItem = (
-        <li>
-          White: {games[gameId].header.White} Black:{' '}
-          {games[gameId].header.Black}
-          <Link to={`/game/${gameId}`}>
-            <button>Play</button>
-          </Link>
-        </li>
-      );
-      arr.push(listItem);
-    }
-
-    return <ul>{arr}</ul>;
-  };
-
   if (!games) {
     return <p>Loading...</p>;
   }
 
-  return <div>{renderList()}</div>;
+  return (
+    <div className="Home">
+      <section className="Home__container">
+        <GamesList games={games} />
+      </section>
+    </div>
+  );
 }
 
 export default MyGames;
